738848bd944f09a7f9cc32efd1bc3fc166804553
ARBOX128 <118316306+ARBOX128@users.noreply.github.com>
2023-08-17 16:34:50 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+video = "";
+status = "";
+objects = "";
+function preload(){
+    video = createVideo('video.mp4');
+    video.hide();
+}
+function setup(){
+    canvas = createCanvas(480,380);
+    canvas.center();
+}
+
+function draw(){
+    image(video, 0,0,480,380);
+    if(status != "")
+    {
+        objectDetector.detect(video, goResult);
+        for (i = 0; i < objects.lenght; i++)
+        document.getElementById("status").innerHTML = "estado: objetos detectados";
+    document.getElementById("number_of_objects").innerHTML = "numero de objetos detectados = "+ objects.lenght;
+
+    fill("FF0000");
+    percent = floor(objects[i].confidence * 100);
+    text(objects[i].label + " " + percent + "%",objects[i].x + 15, objects[i].y + 15);
+    noFill();
+    stroke("FF0000");
+    rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+    }
+}
+function goResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Estado: Detectando objetos";
+}
+
+function modelLoaded(){
+    console.log("Modelo cargado");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volumen(0);
+}
